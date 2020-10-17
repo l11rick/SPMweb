126353a42f6d627806e016bb192f86ca629dbca3
Xudong Zhang <xudongz1@student.unimelb.edu.au>
2020-10-17 15:01:14 +0800

    change firebase project

--- registerPage.js
+++ registerPage.js
@@ -1,3 +1,5 @@
+const db = firebase.firestore();
+
 function register() {
     Email = document.getElementById("email_register").value;
     Password = document.getElementById("password_register").value;
@@ -5,7 +7,6 @@ function register() {
         firebase.auth().createUserWithEmailAndPassword(Email, Password)
             .then(function() {
                 alert("register successful")
-                window.location.href = 'mainPage.html'
             })
             .catch(function(error) {
                 // Handle Errors here.
@@ -16,4 +17,32 @@ function register() {
     } else {
         alert("cant be empty")
     }
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            db.collection("users").doc(user.uid).set({
+                    email: Email,
+                    extra: document.getElementById("extra_register").value,
+                    homeAddress: document.getElementById("address_register").value,
+                    name: document.getElementById("name_register").value,
+                    password: Password,
+                    phone: document.getElementById("phone_register").value
+                })
+                .then(function() {
+                    console.log("Document successfully written!");
+                })
+                .catch(function(error) {
+                    console.error("Error writing document: ", error);
+                });
+
+            db.collection("users").get().then(snapshot => {
+                console.log(snapshot.docs)
+            })
+
+
+            /*window.location.href = 'mainPage.html'*/
+
+        } else {}
+    });
 }
