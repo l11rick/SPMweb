4d974258639d029c31003bcb03aad00266edf2a7
l11 <l11@student.unimelb.edu.au>
2020-10-21 19:36:56 +0800

    1.booking desmonstation complete 2.delete button complete 3.Admin can veiw all booking

--- AdminmainPage.js
+++ AdminmainPage.js
@@ -9,3 +9,26 @@ function signOutAdminMainPage() {
         alert("cannot signout")
     });
 }
+const db = firebase.firestore();
+
+db.collection("bookings").get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.data());
+        $('#view_area').append(`
+        <div class = "card"> 
+            <p font-family:verdana">${doc.data().customerName}</p>
+            <p font-family:verdana">${doc.data().Date}</p>
+            <p font-family:verdana">${doc.data().Time}</p>
+            <p font-family:verdana">${doc.data().Service}</p>
+            <button type="button" onclick = "delete();">Delete</button> 
+        </div>
+        `)
+    });
+});
+
+
+
+
+
+
--- mainPage.js
+++ mainPage.js
@@ -9,3 +9,30 @@ function signOutMainPage() {
         alert("cannot signout")
     });
 }
+const db = firebase.firestore();
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        db.collection("bookings").where("UID", "==", user.uid).get().then(function (snapshot) {
+            snapshot.forEach(function (doc) {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.data());
+                $(`#view_area`).append(`
+                    <div class = "card">
+                    <p font-family:verdana">${doc.data().customerName}</p>
+                    <p font-family:verdana">${doc.data().Date}</p>
+                    <p font-family:verdana">${doc.data().Time}</p>
+                    <p font-family:verdana">${doc.data().Service}</p>
+                    <button type="button" onclick = "delete();">Delete</button>               
+                    </div>
+                `)
+                //renderbook(doc)
+
+
+            });
+        })
+            .catch(function (error) {
+                console.log("Error getting documents: ", error);
+            });
+    };
+})
