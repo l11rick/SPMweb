bc25edc6088c545fead94fb3a3bfeeb3e721f909
Xudong Zhang <xudongz1@student.unimelb.edu.au>
2020-10-22 10:22:59 +0800

    add delete booking function (customer)

--- mainPage.js
+++ mainPage.js
@@ -19,11 +19,14 @@ firebase.auth().onAuthStateChanged(function(user) {
                 console.log(doc.data());
                 $(`#view_area`).append(`
                     <div class = "card">
-                    <p font-family:verdana">${doc.data().customerName}</p>
-                    <p font-family:verdana">${doc.data().Date}</p>
-                    <p font-family:verdana">${doc.data().Time}</p>
-                    <p font-family:verdana">${doc.data().Service}</p>
-                    <button type="button" onclick = "delete();">Delete</button>               
+                    <p font-family:verdana">Name: -- ${doc.data().customerName}</p>
+                    <p font-family:verdana">Home Address: -- ${doc.data().homeAddress}</p>
+                    <p font-family:verdana">Phone Number: -- ${doc.data().phoneNumber}</p>
+                    <p font-family:verdana">Date: -- ${doc.data().Date}</p>
+                    <p font-family:verdana">Time: -- ${doc.data().Time}</p>
+                    <p font-family:verdana">Service: -- ${doc.data().Service}</p>
+                    <p font-family:verdana">${doc.data().Message}</p>
+                    <button type="button" onclick = "deleteBooking('${doc.data().Date}','${doc.data().Time}');">Delete</button>               
                     </div>
                 `)
                 //renderbook(doc)
@@ -36,3 +39,21 @@ firebase.auth().onAuthStateChanged(function(user) {
             });
     };
 })
+
+function deleteBooking(date,time) {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            db.collection("bookings").doc(user.uid+"_"+date+"_"+time).delete().then(function() {
+                console.log("Document successfully deleted!");
+                location.reload()
+            }).catch(function(error) {
+                console.error("Error removing document: ", error);
+            });
+        } else {
+            alert("not logged in")
+            window.location.href = 'loginPage.html'
+        }
+    });
+    
+}
